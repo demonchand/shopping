8620122741f6a34271d2d35fe1affef705446cf7
chandru <chandru@xtrafin.net>
2011-03-09 11:24:45 +0530

    relation ships done to all models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
   default_scope :order => "title"
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :image_url, :description, :presence => true
   validates :title, :uniqueness => true
   validates :image_url, :format => {
@@ -7,4 +12,13 @@ class Product < ActiveRecord::Base
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, :numericality => {:greater_then_or_equal_to => 0.01}
+
+  def ensure_not_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Item present"
+      return false
+    end
+  end
 end
