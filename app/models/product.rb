e836937bcd2e6c0a6c7750d63a285caa91cd8a1f
chandru <chandru@xtrafin.net>
2011-03-09 10:42:28 +0530

    validation is done to Products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :image_url, :description, :presence => true
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, :numericality => {:greater_then_or_equal_to => 0.01}
 end
