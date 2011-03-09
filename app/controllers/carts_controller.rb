5b0294e5faf84f64c3bb12026a513ff0eb57475e
chandru <chandru@xtrafin.net>
2011-03-09 14:05:22 +0530

    after deleting the cart its redirect to store page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -81,7 +81,7 @@ class CartsController < ApplicationController
     @cart.destroy
 
     respond_to do |format|
-      format.html { redirect_to(carts_url) }
+      format.html { redirect_to(root_url) }
       format.xml  { head :ok }
     end
   end
