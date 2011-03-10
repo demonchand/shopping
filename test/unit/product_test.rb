c4ab6f3f32fe0373b284f5ba28696863fa70e421
chandru <chandru@xtrafin.net>
2011-03-10 09:52:13 +0530

    fixture is deleted and factory girl is added

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+class ProductTest < Test::Unit::TestCase
+  should have_many(:line_items)
+  should validate_presence_of(:title)
+
+ # describe Product do
+ #   it { should have_many(:line_items) }
+ #   it { should validate_presence_of(:title) }
+ # end
 end
+
